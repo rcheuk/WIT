7164b1c66e30cb4fe5ab0c97e45e059caf861df8
Evann <evann.g.smith@gmail.com>
2017-07-21 16:03:20 -0400

    Simple web app

--- recommend_api.py
+++ recommend_api.py
@@ -1,46 +1,62 @@
-import json
-from flask import Flask, request
+from flask import Flask, render_template, request, url_for, Markup
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, SelectField
 
-from recommend_words import get_most_similar, read_text, process_text, get_gendered_words, highlight_gendered_words
+from recommend_words import get_most_similar, process_text, get_gendered_words, highlight_gendered_words
+
+class DocumentForm(FlaskForm):
+  text = TextAreaField("Enter document text to analyze:")
+  submitdoc = SubmitField("Analyze")
+
+class WordForm(FlaskForm):
+  word = StringField("Enter a word or two word phrase to get recommendations:")
+  strongly_male = BooleanField("Strongly Male")
+  male = BooleanField("Male")
+  neutral = BooleanField("Neutral")
+  female = BooleanField("Female")
+  strongly_female = BooleanField("Strongly Female")
+  pos = SelectField('Part of Speech', choices=[('any', 'Any'), ('NOUN', 'Noun'), ('VERB', 'Verb'),
+                                                      ('ADJ', 'Adjective'), ('ADV', 'Adverb')])
+  submitword = SubmitField("Recommend")
 
 app = Flask(__name__)
+app.secret_key = 'development key'
 
 @app.route('/')
-def hello():
-	return 'Hello World'
+def forms():
+    document_form = DocumentForm()
+    word_form = WordForm()
+    return render_template('form_submit.html', document_form=document_form, word_form=word_form)
 
-@app.route('/recommend')
+@app.route('/recommend', methods=['POST'])
 def recommend():
     try:
-        word = request.args.get('word')
-        topn = request.args.get('topn', default=10, type=int)
-        gender = request.args.get('gender', default=None)
-        pos = request.args.get('pos', default=None)
-
-        if gender is not None:
-            gender = gender.split(',')
-
-        if pos is not None:
-            pos = pos.split(',')
+        word = request.form['word'].replace(' ', '_')
+        gender = [g for g in ['strongly_male', 'male', 'neutral', 'female', 'strongly_female'] if g in request.form]
+        if len(gender) == 0 or len(gender) == 5:
+            gender = None
+        pos = request.form['pos']
+        if pos == 'any':
+            pos = None
+        topn = 10
 
         recommendations = get_most_similar(word, topn=topn, gender=gender, pos=pos)
-        return json.dumps({'recommendations': recommendations}, ensure_ascii=False)
+        return render_template('word_recommendations.html', recommendations=recommendations)
 
     except Exception as e:
-        return json.dumps({'error': str(e)}, ensure_ascii=False)
+        return render_template('error.html', error=str(e))
 
-@app.route('/analyze_document')
+@app.route('/analyze_document', methods=['POST'])
 def analyze_document():
     try:
-        file = request.args.get('file')
-        text = read_text(file)
+        text = request.form['text']
         processed_text = process_text(text)
         word_data = get_gendered_words(processed_text)
         highlighted_text = highlight_gendered_words(text, word_data)
-        return json.dumps({'highlighted_text': str(highlighted_text)}, ensure_ascii=False)
+        return render_template('analyzed_document.html', highlighted_text=Markup(highlighted_text))
 
     except Exception as e:
-        return json.dumps({'error': str(e)}, ensure_ascii=False)
+        return render_template('error.html', error=str(e))
 
 if __name__ == "__main__":
-	app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000)
